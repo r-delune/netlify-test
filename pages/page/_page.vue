394a4458e0ebbbe2a3cd99800335b78dc5d37dc5
Bri <bdillon@live.ie>
2020-05-21 18:50:44 +0100

    first commit

---
+++ pages/page/_page.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <h1>{{blogPost.title}}</h1>
+    <div v-html="$md.render(blogPost.body)" />
+  </article>
+</template>
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { pageData: payload }
+    else
+      return {
+        pageData: await require(`~/assets/content/page/${params.page}.json`)
+      }
+  }
+}
+</script>
