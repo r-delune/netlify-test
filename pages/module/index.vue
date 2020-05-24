c7d3a4292724b607d834757fcf3943dc9bb95d9d
Bri <bdillon@live.ie>
2020-05-24 19:48:08 +0100

    page

--- pages/module/index.vue
+++ pages/module/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ul v-for="(m, index) in modules" :key="index">
-      <nuxt-link :to="`blog/${m.title}`">{{m.title}}</nuxt-link>
+      <nuxt-link :to="`/${m.title}`">{{m.title}}</nuxt-link>
       <p>{{m.page}}</p>
     </ul>
   </div>
