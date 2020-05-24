a96b8d5b3448f8d21508d871e1c57ada939c81bf
Bri <bdillon@live.ie>
2020-05-24 21:35:43 +0100

    update

--- components/input/BinarySelect.vue
+++ components/input/BinarySelect.vue
@@ -1,58 +1,53 @@
-<template>  
-    <div class="card-body text-center">                 
-        <div class="row justify-content-center align-items-center">
-            <div class="btn-wrapper">
-                <base-button tag="a" 
-                @click.end="select('Yes', 0)"
-                :class="{'active': isClicked[0]}"
-                size="sm"
-                >
-                    Yes
-                </base-button>
-                <base-button tag="a"
-                @click.end="select('No', 1)"
-                :class="{'active': isClicked[1]}"
-                size="sm"
-                >
-                    No
-                </base-button>
-            </div>
-        </div>
+<template>
+  <div class="card-body text-center">
+    <div class="row justify-content-center align-items-center">
+      <div class="btn-wrapper">
+        <base-button
+          tag="a"
+          @click.end="select('Yes', 0)"
+          :class="{'active': isClicked[0]}"
+          size="sm"
+        >Yes</base-button>
+        <base-button
+          tag="a"
+          @click.end="select('No', 1)"
+          :class="{'active': isClicked[1]}"
+          size="sm"
+        >No</base-button>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: "binary-select",
-    data: () => ({
-        selected_items: [],
-        isClicked: [0,0]
-    }),
-    props: {
-        items: Array
-    },
-    methods: {
-        select (item, index) {
+  name: 'binary-select',
+  data: () => ({
+    selected_items: [],
+    isClicked: [0, 0]
+  }),
+  props: {
+    items: Array
+  },
+  methods: {
+    select(item, index) {
+      this.$set(this.isClicked, index, this.isClicked[index])
 
-            this.$set(this.isClicked, index, this.isClicked[index])
+      if (index == 0) {
+        this.$set(this.isClicked, 0, !this.isClicked[0])
+        //this.$set(this.isClicked, 0, !this.isClicked[1])
+      } else {
+        this.$set(this.isClicked, 1, !this.isClicked[1])
+        //this.$set(this.isClicked, 0, this.isClicked[0])
+      }
 
-            if (index == 0) {
-                this.$set(this.isClicked, 0, !this.isClicked[0])
-                //this.$set(this.isClicked, 0, !this.isClicked[1])
-            }
-          else {
-                this.$set(this.isClicked, 1, !this.isClicked[1])
-                //this.$set(this.isClicked, 0, this.isClicked[0])
-            }
-           
-            console.log('inner selected answer ' + item)
-            this.$emit('selected', item)
-        }
-    },
-    
+      console.log('inner selected answer ' + item)
+      this.$emit('selected', item)
+    }
+  }
 }
 </script>
 <style scoped>
-    .active {
-        color: white
-    }
+.active {
+  color: white;
+}
 </style>
--- pages/module/_module.vue
+++ pages/module/_module.vue
@@ -11,15 +11,15 @@
       <div v-if="moduler.page[this.page_number].form.length > 0">
         <!-- Yes/No Question -->
         <div v-if="moduler.page[this.page_number].form.type == 'binary'">
-          <binary @selected="selected"></binary>
+          <binary :questions="questions" @selected="selected"></binary>
         </div>
         <!-- Slider Question -->
         <div v-if="moduler.page[this.page_number].form.type == 'slider'">
-          <slider @selected="selected"></slider>
+          <!-- <slider @selected="selected"></slider> -->
         </div>
         <!-- Rank Question -->
         <div v-if="moduler.page[this.page_number].form.type == 'rank'">
-          <rank @selected="selected"></rank>
+          <!-- <rank @selected="selected"></rank> -->
         </div>
         <!-- Choice Question -->
         <div v-if="moduler.page[this.page_number].form.type == 'choice'">
@@ -27,7 +27,7 @@
         </div>
         <!-- Custom Question -->
         <div v-if="moduler.page[this.page_number].form.type == 'custom'">
-          <binary-select @selected="selected"></binary-select>
+          <!-- <binary-select @selected="selected"></binary-select> -->
         </div>
         <div class="text-center">
           <button @click="nextPage(page+1)">next</button>
@@ -41,17 +41,28 @@
   </transition>
 </template>
 <script>
-import Binary from '~/components/input/BinarySelect'
-import MultipleChoice from '~/components/input/MultipleChoice'
-import MultipleChoiceSingle from '~/components/input/MultipleChoiceSingle'
-import MultiBinarySelect from '~/components/input/MultiBinarySelect'
-import MultiSlider from '~/components/input/BinarySelect'
+import Binary from '~/components/input/BinarySelect.vue'
+import MultipleChoice from '~/components/input/MultipleChoice.vue'
+// import MultipleChoiceSingle from '~/components/input/MultipleChoiceSingle.vue'
+// import MultiBinarySelect from '~/components/input/MultiBinarySelect.vue'
+// import MultiSlider from '~/components/input/BinarySelect.vue'
 
-import CustomExample from '~/components/custom/CustomExample'
+import CustomExample from '~/components/custom/CustomExample.vue'
 
 export default {
   data: () => ({
-    page_number: 0
+    page_number: 0,
+    questions: [
+      'Do you abuse more than one drug at a time?',
+      'Are you always able to stop using drugs when you want to?',
+      'Have you ever had blackouts or flashbacks as a result of drug use?',
+      'Do you ever feel bad or guilty about your drug use?',
+      'Does your spouse (or parents) ever complain about your involvement with drugs?',
+      'Have you neglected your family because of your use of drugs?',
+      'Have you engaged in illegal activities in order to obtain drugs?',
+      'Have you ever experienced withdrawal symptoms (felt sick) when you stopped taking drugs?',
+      'Have you had medical problems as a result of your drug use (e.g. memory loss, hepatitis, convulsions, bleeding)?'
+    ]
   }),
   computed: {
     page() {
@@ -59,11 +70,11 @@ export default {
     }
   },
   components: {
-    MultiSlider,
+    // MultiSlider,
     Binary,
     MultipleChoice,
-    MultipleChoiceSingle,
-    MultiBinarySelect,
+    // MultipleChoiceSingle,
+    // MultiBinarySelect,
     CustomExample
   },
   methods: {
